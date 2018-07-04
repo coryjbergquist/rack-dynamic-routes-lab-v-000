9082dbe15d7dbe8cd4a4c4880765a5fdd2f6612f
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-04 16:39:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,17 @@
 class Application
 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ binding.pry
+    if req.path.match(/items/)
+      resp.write "You requested the songs"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
 end
