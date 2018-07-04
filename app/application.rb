94b6c026dee2a1b410fb9a7321d23cea3d0e58fe
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-04 17:00:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     req = Rack::Request.new(env)
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if @@all.include?(item_name)
+      if item.all.include?(item_name)
         binding.pry
       end
 
