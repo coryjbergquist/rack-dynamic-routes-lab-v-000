a37dab20c1fa832a34f403c4ee39bc28f3f8db7c
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-04 17:24:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,6 @@ class Application
           # resp.write "Item not found"
           # resp.status = 400
         end
-      end
     else
       resp.write "Route not found"
       resp.status = 404
